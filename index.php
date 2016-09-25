934dd90c99ef0375ba21c336b21c45daaf3c334b
EvgeniyFominTheLoop <aristocratic@i.ua>
2016-09-25 21:36:48 +0300

    lyceum

---
+++ index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    	<meta charset="UTF-8" />
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<meta name="keywords" content="хал, хду, хгу, мишуков, мішуков, лицей, ліцей" name="description" content="Херсонський академічний ліцей імені О.В. Мішукова Херсонської міської ради при Херсонському державному університеті">
+    	<meta name="author" content="Evgeniy Fomin" />
+    	<title>Херсонський академічний ліцей імені О.В. Мішукова</title>
+
+    	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    	<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link rel="stylesheet" type="text/css" href="css/font-awesome.css" />
+        <link rel="stylesheet" type="text/css" href="css/slider.css" />
+		<script type="text/javascript" src="js/modernizr.custom.79639.js"></script>
+		<noscript>
+			<link rel="stylesheet" type="text/css" href="css/styleNoJS.css" />
+		</noscript>
+
+
+
+    	
+    </head>
+    <!-- Содержание страницы -->
+    <body>
+    <header>
+    	<div id="menu-wrapper">
+			<ul class="menu">
+				<li><a href="http://lyceum.com.ua/"><i class="fa fa-home"></i>ГОЛОВНА</a></li>
+				<li><a href="/litsejistam_ta_batkam"><i class="fa fa-users"></i>ЛІЦЕЇСТАМ ТА БАТЬКАМ</a></li>
+				<li><a href="/nashi_proekti"><i class="fa fa-envira"></i>НАШІ ПРОЕКТИ</a></li>
+				<li><a href="http://pv.lyceum.com.ua/" target="_blank"><i class="fa fa-child"></i>ПІДГОТОВЧЕ ВІДДІЛЕННЯ</a></li>
+				<li><a href="/rozkladi"><i class="fa fa-calendar"></i>РОЗКЛАД</a></li>
+				<li><a href="http://lyceum.com.ua/klasna_otsinka" target="_blank"><i class="fa fa-book"></i>КЛАСНА ОЦІНКА</a></li>
+				<li><a href="/gallery"><i class="fa fa-camera"></i>ФОТОГАЛЕРЕЯ</a></li>
+				<li><a href="/litsej_times"><i class="fa fa-graduation-cap"></i>OWL NEWS</a></li>
+			</ul>
+    	</div>
+ 	
+		<div id="slider">
+			<div class="sl-slider">
+			
+				<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+					<div class="sl-slide-inner">
+						<div class="bg-img bg-img-1"></div>
+						<h2>Херсонський академічний ліцей імені О.В. Мішукова</h2>
+						<blockquote><p>Херсонської міської ради при Херсонському державному університеті</p></blockquote>
+					</div>
+				</div>
+				
+				<div class="sl-slide" data-orientation="vertical" data-slice1-rotation="10" data-slice2-rotation="-15" data-slice1-scale="1.5" data-slice2-scale="1.5">
+					<div class="sl-slide-inner">
+						<div class="bg-img bg-img-2"></div>
+						<h2>Елітна освіта - гарантована кар'єра</h2>
+					</div>
+				</div>
+				
+				<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="3" data-slice2-rotation="3" data-slice1-scale="2" data-slice2-scale="1">
+					<div class="sl-slide-inner">
+						<div class="bg-img bg-img-3"></div>
+						<h2>Елітна освіта - гарантована кар'єра</h2>
+					</div>
+				</div>
+				
+				<div class="sl-slide" data-orientation="vertical" data-slice1-rotation="-5" data-slice2-rotation="25" data-slice1-scale="2" data-slice2-scale="1">
+					<div class="sl-slide-inner">
+						<div class="bg-img bg-img-4"></div>
+						<h2>Елітна освіта - гарантована кар'єра</h2>
+					</div>
+				</div>
+				
+				<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1">
+					<div class="sl-slide-inner">
+						<div class="bg-img bg-img-5"></div>
+						<h2>Елітна освіта - гарантована кар'єра</h2>
+					</div>
+				</div>
+			</div><!-- /sl-slider -->
+
+			<nav id="nav-dots" class="nav-dots">
+				<span class="nav-dot-current"></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+			</nav>
+
+		</div><!-- slider -->
+    </header>
+
+    <!-- СЛАЙДЕР -->
+
+    	<p id="temp">
+    		Що таке Lorem Ipsum?
+Lorem Ipsum - це текст-"риба", що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично, стандартною "рибою" аж з XVI сторіччя, коли невідомий друкар взяв шрифтову гранку та склав на ній підбірку зразків шрифтів. "Риба" не тільки успішно пережила п'ять століть, але й прижилася в електронному верстуванні, залишаючись по суті незмінною. Вона популяризувалась в 60-их роках минулого сторіччя завдяки виданню зразків шрифтів Letraset, які містили уривки з Lorem Ipsum, і вдруге - нещодавно завдяки програмам комп'ютерного верстування на кшталт Aldus Pagemaker, які використовували різні версії Lorem Ipsum.
+
+Чому ми ним користуємось?
+Вже давно відомо, що читабельний зміст буде заважати зосередитись людині, яка оцінює композицію сторінки. Сенс використання Lorem Ipsum полягає в тому, що цей текст має більш-менш нормальне розподілення літер на відміну від, наприклад, "Тут іде текст. Тут іде текст." Це робить текст схожим на оповідний. Багато програм верстування та веб-дизайну використовують Lorem Ipsum як зразок і пошук за терміном "lorem ipsum" відкриє багато веб-сайтів, які знаходяться ще в зародковому стані. Різні версії Lorem Ipsum з'явились за минулі роки, деякі випадково, деякі було створено зумисно (зокрема, жартівливі).
+
+
+Звідки він походить?
+На відміну від поширеної думки Lorem Ipsum не є випадковим набором літер. Він походить з уривку класичної латинської літератури 45 року до н.е., тобто має більш як 2000-річну історію. Річард Макклінток, професор латини з коледжу Хемпдін-Сидні, що у Вірджінії, вивчав одне з найменш зрозумілих латинських слів - consectetur - з уривку Lorem Ipsum, і у пошуку цього слова в класичній літературі знайшов безсумнівне джерело. Lorem Ipsum походить з розділів 1.10.32 та 1.10.33 цицеронівського "de Finibus Bonorum et Malorum" ("Про межі добра і зла"), написаного у 45 році до н.е. Цей трактат з теорії етики був дуже популярним в епоху Відродження. Перший рядок Lorem Ipsum, "Lorem ipsum dolor sit amet..." походить з одного з рядків розділу 1.10.32.
+
+Класичний текст, використовуваний з XVI сторіччя, наведено нижче для всіх зацікавлених. Також точно за оригіналом наведено розділи 1.10.32 та 1.10.33 цицеронівського "de Finibus Bonorum et Malorum" разом із перекладом англійською, виконаним 1914 року Х.Рекемом.
+
+Де собі взяти трохи?
+Існує багато варіацій уривків з Lorem Ipsum, але більшість з них зазнала певних змін на кшталт жартівливих вставок або змішування слів, які навіть не виглядають правдоподібно. Якщо ви збираєтесь використовувати Lorem Ipsum, ви маєте упевнитись в тому, що всередині тексту не приховано нічого, що могло б викликати у читача конфуз. Більшість відомих генераторів Lorem Ipsum в Мережі генерують текст шляхом повторення наперед заданих послідовностей Lorem Ipsum. Принципова відмінність цього генератора робить його першим справжнім генератором Lorem Ipsum. Він використовує словник з більш як 200 слів латини та цілий набір моделей речень - це дозволяє генерувати Lorem Ipsum, який виглядає осмислено. Таким чином, згенерований Lorem Ipsum не міститиме повторів, жартів, нехарактерних для латини слів і т.ін.
+    	</p>
+
+
+
+
+
+
+    	<script type="text/javascript" src="js/jquery-3.1.1.js"></script>
+        <script type="text/javascript" src="js/bootstrap.js"></script>
+
+
+
+
+
+
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		<script type="text/javascript" src="js/jquery.ba-cond.min.js"></script>
+		<script type="text/javascript" src="js/jquery.slitslider.js"></script>
+		<script type="text/javascript">	
+			$(function() {
+			
+				var Page = (function() {
+
+					var $nav = $( '#nav-dots > span' ),
+						slitslider = $( '#slider' ).slitslider( {
+							onBeforeChange : function( slide, pos ) {
+
+								$nav.removeClass( 'nav-dot-current' );
+								$nav.eq( pos ).addClass( 'nav-dot-current' );
+
+							}
+						} ),
+
+						init = function() {
+
+							initEvents();
+							
+						},
+						initEvents = function() {
+
+							$nav.each( function( i ) {
+							
+								$( this ).on( 'click', function( event ) {
+									
+									var $dot = $( this );
+									
+									if( !slitslider.isActive() ) {
+
+										$nav.removeClass( 'nav-dot-current' );
+										$dot.addClass( 'nav-dot-current' );
+									
+									}
+									
+									slitslider.jump( i + 1 );
+									return false;
+								
+								} );
+								
+							} );
+
+						};
+
+						return { init : init };
+
+				})();
+
+				Page.init();
+			
+			});
+		</script>
+
+
+    </body>
+</html>
