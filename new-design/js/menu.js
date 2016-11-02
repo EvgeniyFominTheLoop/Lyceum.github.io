f5d91f82e614663452eb6285db924056386dd128
EvgeniyFominTheLoop <aristocratic@i.ua>
2016-11-02 16:24:14 +0200

    1

--- new-design/js/menu.js
+++ new-design/js/menu.js
@@ -5,17 +5,17 @@
 var toggles = document.querySelectorAll(".mobile-menu"); 
 
 for (var i = toggles.length - 1; i >= 0; i--) { 
-var toggle = toggles[i]; 
-toggleHandler(toggle); 
+	var toggle = toggles[i]; 
+	toggleHandler(toggle); 
 }; 
 
 function toggleHandler(toggle) { 
-toggle.addEventListener( "click", function(e) { 
-e.preventDefault(); 
-var burger = this.getElementsByClassName('c-hamburger'); 
-if ( burger.length > 0){ 
-(burger[0].classList.contains("is-active") === true) ? burger[0].classList.remove("is-active") : burger[0].classList.add("is-active"); 
-} 
+	toggle.addEventListener( "click", function(e) { 
+		e.preventDefault(); 
+		var burger = this.getElementsByClassName('c-hamburger'); 
+			if ( burger.length > 0){ 
+				(burger[0].classList.contains("is-active") === true) ? burger[0].classList.remove("is-active") : burger[0].classList.add("is-active"); 
+	} 
 }); 
 } 
 
@@ -26,14 +26,14 @@ var touch = $('.mobile-menu');
 var menu = $('.menu'); 
 
 $(touch).on('click', function(e) { 
-e.preventDefault(); 
-$(e.target).siblings('.menu').slideToggle(); 
+	e.preventDefault(); 
+	$(e.target).siblings('.menu').slideToggle(); 
 }); 
 
 $(window).resize(function(){ 
-var w = $(window).width(); 
-if(w > 767 && menu.is(':hidden')) { 
-menu.removeAttr('style'); 
+	var w = $(window).width(); 
+	if(w > 767 && menu.is(':hidden')) { 
+		menu.removeAttr('style'); 
 } 
 }); 
 
