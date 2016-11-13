97ff7c6b9a093a9829d3a3e4e2dd06e65626c753
EvgeniyFominTheLoop <aristocratic@i.ua>
2016-11-14 01:17:57 +0200

    1

--- new-design/old_des/tpl/frontend/news/show.php
+++ new-design/old_des/tpl/frontend/news/show.php
@@ -14,4 +14,24 @@
 <div class="text-center news-footer">
 	<a href="/news<?php echo(TYPE_NEWS==1 ? "/type/".$obj->type : "")?>">Всі новини <i class="fa fa-arrow-circle-right"></i></a>
 </div>
+<div id="disqus_thread"></div>
+<script>
+
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+
+var disqus_config = function () {
+this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = '//academlyceum.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 		
