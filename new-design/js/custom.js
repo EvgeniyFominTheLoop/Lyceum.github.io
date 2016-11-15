1510e47395d314dc97009364d155d08f852be068
EvgeniyFominTheLoop <aristocratic@i.ua>
2016-11-15 15:44:23 +0200

    1

--- new-design/js/custom.js
+++ new-design/js/custom.js
@@ -35,15 +35,15 @@ $(function () {
 })
 
 // ПАНЕЛЬ СООБЩЕНИЯ
-// function viewdiv(id){
-// 	var el=document.getElementById(id);
-// 	if(el.style.display=="block"){
-// 		el.style.display="none";
-// 	}
-// 	else {
-// 		el.style.display="block";
-// 	}
-// }
+function viewdiv(id){
+	var el=document.getElementById(id);
+	if(el.style.display=="block"){
+		el.style.display="none";
+	}
+	else {
+		el.style.display="block";
+	}
+}
 
 // ЗАПРЕТ НА ПЕРЕТАСКИВАНИЕ КАРТИНОК
 $('img').on('dragstart', function(event) { event.preventDefault(); });
