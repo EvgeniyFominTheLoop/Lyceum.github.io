2c23bf84b356dee9c969c243ec0a685ba1cecb55
EvgeniyFominTheLoop <aristocratic@i.ua>
2016-11-17 04:45:05 +0200

    1

--- new-design/js/menu.js
+++ new-design/js/menu.js
@@ -1,23 +1,3 @@
-(function() { 
-"use strict"; 
-var toggles = document.querySelectorAll(".mobile-menu"); 
-for (var i = toggles.length - 1; i >= 0; i--) { 
-	var toggle = toggles[i]; 
-	toggleHandler(toggle); 
-}; 
-
-function toggleHandler(toggle) { 
-	toggle.addEventListener( "click", function(e) { 
-		e.preventDefault(); 
-		var burger = this.getElementsByClassName('c-hamburger'); 
-			if ( burger.length > 0){ 
-				(burger[0].classList.contains("is-active") === true) ? burger[0].classList.remove("is-active") : burger[0].classList.add("is-active"); 
-	} 
-}); 
-} 
-
-})();
-
 $(document).ready(function(){
 var touch = $('.mobile-menu');
 var menu = $('.menu');
@@ -25,6 +5,7 @@ var menu = $('.menu');
 $(touch).on('click', function(e) {
 	e.preventDefault();
 	$(e.target).siblings('.menu').slideToggle();
+	$(".hamburger ul").toggleClass('active');
 }); 
 
 $(window).resize(function(){ 
